fa1b0e04d2e84204addfaa93d15577379a2a604b
arshad ansari <arshadchennaisbg.apj.369@gmail.com>
2023-01-27 18:10:01 +0530

    In this added 'RegisterActity' -> conected the entered credentials whth FIREBASE (only in RegisterActivity)

--- app/src/main/java/com/example/loginapplication/MainActivity.java
+++ app/src/main/java/com/example/loginapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.loginapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,16 +12,19 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Button register_btn = findViewById(R.id.register_btn);
         EditText txtView =  findViewById(R.id.username);
         EditText pswrd = findViewById(R.id.password);
-        Button btn = findViewById(R.id.loginbtn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button login_btn = findViewById(R.id.loginbtn);
+
+        //for login
+        login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(txtView.getText().toString().equals("Arshad") && pswrd.getText().toString().equals("1436"))
@@ -29,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "INCORRECT CREDENTIALS", Toast.LENGTH_LONG).show();
             }
         });
+
+        //for register
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/loginapplication/RegisterActivity.java
@@ -0,0 +1,66 @@
+package com.example.loginapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView userName;
+    TextView pswrd;
+    Button reg_btn;
+
+    //this is an auth of Firebase
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        userName = findViewById(R.id.register_username);
+        pswrd = findViewById(R.id.register_password);
+        reg_btn = findViewById(R.id.register_btn);
+
+        auth = FirebaseAuth.getInstance();
+
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = userName.getText().toString();
+                String password = pswrd.getText().toString();
+
+                //see if credentials entered or not
+                if(name.isEmpty() && password.isEmpty())
+                    Toast.makeText(RegisterActivity.this, "Enter Credentials", Toast.LENGTH_LONG).show();
+                else if (password.length() < 6)
+                    Toast.makeText(RegisterActivity.this, "Password length is Short", Toast.LENGTH_SHORT).show();
+                else     //if everything is ok -> register user in firebase by calling this function
+                    registerUser(name, password);
+            }
+        });
+
+    }
+
+    private void registerUser(String name, String password) {
+        //call this method "createUserWithEmailAndPassword" and pass those credentials, and then add
+        //another method "addOnComplete" to show user a message of register is completed or not
+        auth.createUserWithEmailAndPassword(name, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
